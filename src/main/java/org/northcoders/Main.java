477626716830fd8466d843fa46dc60764eb0e7c4
Tawanda Chabva <tchabva@gmail.com>
2024-11-06 08:42:32 +0000

    first commit

---
+++ src/main/java/org/northcoders/Main.java
@@ -0,0 +1,40 @@
+package org.northcoders;
+
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // List - A linear collection of individual elements
+        List<String> stringList = List.of("1", "2", "3");
+
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add("Hi");
+
+        //Array
+        String[] strings = new String[0];
+
+        // Map
+        Map<Integer, String> stringMap = Map.of();
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1,"hello");
+        hashMap.get(1);
+
+
+        // Set
+        Set<String> stringSet;
+
+        // Stack
+        // Queues
+
+        // Implementations
+        // - ArrayList (List)
+        // - LinkedList (List)
+        // - Vector (List)
+        // - HashMap (Map)
+
+        // Structures
+        // - Tree
+    }
+}
